da728438fe2a7b11acdf590aae9fddfb48a386ab
Mohsen <s.m.bakamal1385@gmail.com>
2026-06-21 19:16:12 +0330

    Make some changes in messages.
    Some changes like use Markdown in start and options commands.

--- core/main.py
+++ core/main.py
@@ -9,22 +9,23 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-        سلام. خیلی خوش اومدی.
-من رصدخان هستم. از آشنایی باهات خوشحالم. 😄🤍
-""")
+    first_name = message.from_user.first_name
+    bot.reply_to(message, f"""\
+        سلام {first_name}. خیلی خوش اومدی.
+من *رصدخــــان* هستم. از آشنایی باهات خوشحالم. 😄🤍
+""", parse_mode='Markdown')
     
 # Handle '/options'
 @bot.message_handler(commands=['options'])
 def send_options(message):
     bot.send_message(message.chat.id, """\
-        رصدخــــان این‌جاست تا از پس کارهای متنوعی بربیاد! 😎
+        *رصدخــــان* این‌جاست تا از پس کارهای متنوعی بربیاد! 😎
                      
-قابلیت‌های فعلی من:
+*قابلیت‌های فعلی من*:
 🔗 دریافت لینک کانال رصد علم و صنعت در پیام‌رسان‌های مختلف
 📚 پیشنهاد دروسی که قراره در تابستون ارائه بشه
 🗣 دریافت نظرات، پیشنهادات و انتقادات شما به‌صورت کاملا ناشناس 
 (حتی می‌تونی بهمون پیشنهاد بدی که جای چی توی ربات خالیه)
-""")
+""", parse_mode='Markdown')
 
 bot.infinity_polling()
