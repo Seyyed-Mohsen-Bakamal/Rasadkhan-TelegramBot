1b40468b96de88232236dd706ba9513e4d6b2f80
Mohsen <s.m.bakamal1385@gmail.com>
2026-06-24 16:31:33 +0330

    Create some buttons for bot
    Currently, the buttons are just for display and have no functionality.

--- core/main.py
+++ core/main.py
@@ -1,6 +1,7 @@
 import telebot
 import os
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 load_dotenv()
 API_TOKEN = os.environ.get('API_TOKEN')
@@ -9,18 +10,20 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start'
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder= 'متن موردنظر خود را بنویسید.')
+    markup.add(KeyboardButton('🔗 لینک‌های رصد'), KeyboardButton('🗣 ارائۀ نظر، پیشنهاد یا انتقاد'))
+    markup.add('📚 پیشنهاد دروس ترم تابستان', '🤔 دانشجوها چی می‌گن؟') #another way to create a button
     first_name = message.from_user.first_name
-    bot.reply_to(message, f"""\
-        سلام {first_name}. خیلی خوش اومدی.
-من *رصدخــــان* هستم. از آشنایی باهات خوشحالم. 😄🤍
-""", parse_mode='Markdown')
-    
+    text = f'''سلام {first_name}. خیلی خوش اومدی.
+من <b>رصدخــــان</b> هستم. از آشنایی باهات خوشحالم. 😄🤍'''
+    bot.reply_to(message, text, parse_mode='HTML', reply_markup = markup)
+
 # Handle '/options'
 @bot.message_handler(commands=['options'])
 def send_options(message):
     bot.send_message(message.chat.id, """\
         *رصدخــــان* این‌جاست تا از پس کارهای متنوعی بربیاد! 😎
-                     
+
 *قابلیت‌های فعلی من*:
 🔗 دریافت لینک کانال رصد علم و صنعت در پیام‌رسان‌های مختلف
 📚 پیشنهاد دروسی که قراره در تابستون ارائه بشه
